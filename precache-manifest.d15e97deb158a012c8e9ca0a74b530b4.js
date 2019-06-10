ea6c6400f6d7454c77f4dfdc818bccefb0a04dd1
kstweather <kst.weather@gmail.com>
2019-06-11 01:36:04 +0900

    Updates

---
+++ precache-manifest.d15e97deb158a012c8e9ca0a74b530b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696df776a7a6f53feb39df287238403d",
+    "url": "/kstweather/index.html"
+  },
+  {
+    "revision": "42805eaf578f1c125a2b",
+    "url": "/kstweather/static/css/main.34f1a3a1.chunk.css"
+  },
+  {
+    "revision": "f4082850001640773ae0",
+    "url": "/kstweather/static/js/2.39d5be67.chunk.js"
+  },
+  {
+    "revision": "42805eaf578f1c125a2b",
+    "url": "/kstweather/static/js/main.989d1e68.chunk.js"
+  },
+  {
+    "revision": "87c31df4f479dce88988",
+    "url": "/kstweather/static/js/runtime~main.81e21d2a.js"
+  },
+  {
+    "revision": "c0e4e2a2e1cc60eeb9b2b7b5a27d2386",
+    "url": "/kstweather/static/media/date0.c0e4e2a2.png"
+  },
+  {
+    "revision": "27adde598e5dd045b2ace3ddf4a667d0",
+    "url": "/kstweather/static/media/date1.27adde59.jpg"
+  },
+  {
+    "revision": "314785cf5a678fd6f2475c1b3ffb8b3c",
+    "url": "/kstweather/static/media/date2.314785cf.jpg"
+  },
+  {
+    "revision": "16bb762185a49ff7a606d75cf5045878",
+    "url": "/kstweather/static/media/date3.16bb7621.jpg"
+  },
+  {
+    "revision": "59de8771228044a1653af2d6ac5a37fb",
+    "url": "/kstweather/static/media/date4.59de8771.jpg"
+  },
+  {
+    "revision": "907c18e5e4984ef2b20f24cd616fb021",
+    "url": "/kstweather/static/media/date5.907c18e5.jpg"
+  },
+  {
+    "revision": "ec119d9f172c4c8f8841c88486472ede",
+    "url": "/kstweather/static/media/date6.ec119d9f.jpg"
+  },
+  {
+    "revision": "094433a9bb78af24af5f3b8a3eaca0ab",
+    "url": "/kstweather/static/media/date7.094433a9.jpg"
+  }
+]);
